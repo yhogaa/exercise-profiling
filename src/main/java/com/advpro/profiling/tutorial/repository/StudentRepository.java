b58d0399f122f4a841219f76e8ed3a9a5dc13c5f
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2024-03-13 19:22:22 +0700

    [Refactoring] - Optimizing method findStudentWithHighestGpa

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -2,11 +2,17 @@ package com.advpro.profiling.tutorial.repository;
 
 import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * @author muhammad.khadafi
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query(value = "SELECT * FROM students ORDER BY gpa DESC LIMIT 1", nativeQuery = true)
+
+    Optional<Student> findStudentWithHighestGpa();
 }
